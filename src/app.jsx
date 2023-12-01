0bb79363ef0d2167457b22648c5a7436e5971c3d
Glitch (glitch-hello-react) <none>
2023-12-01 17:09:22 +0000

    😜👖 Checkpoint ./src/seo.json:59215480/246 ./src/components/router.jsx:59215480/239 ./src/pages/about.jsx:59215480/2495 ./src/pages/home.jsx:59215480/2561 ./src/app.jsx:59215480/1614 ./resume-en.json:59215480/6918 ./resume-pt.json:59215480/7451

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Router, Link } from "wouter";
 
-/**
-* This code defines the react app
-*
-* Imports the router functionality to provide page navigation
-* Defines the Home function outlining the content on each page
-* Content specific to each page (Home and About) is defined in their components in /pages
-* Each page content is presented inside the overall structure defined here
-* The router attaches the page components to their paths
-*/
-
-// Import and apply CSS stylesheet
-import "./styles/styles.css";
-
 // Where all of our pages come from
 import PageRouter from "./components/router.jsx";
 
@@ -25,27 +12,17 @@ export default function Home() {
   return (
     <Router>
       <Seo />
-      <main role="main" className="wrapper">
-        <div className="content">
-          {/* Router specifies which component to insert here as the main content */}
+      <main>
+        <div>
           <PageRouter />
         </div>
       </main>
-      {/* Footer links to Home and About, Link elements matched in router.jsx */}
-      <footer className="footer">
-        <div className="links">
+      <footer>
+        <div>
           <Link href="/">Home</Link>
-          <span className="divider">|</span>
+          <span>|</span>
           <Link href="/about">About</Link>
         </div>
-        <a
-          className="btn--remix"
-          target="_top"
-          href="https://glitch.com/edit/#!/remix/glitch-hello-react"
-        >
-          <img src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140" alt="" />
-          Remix on Glitch
-        </a>
       </footer>
     </Router>
   );
