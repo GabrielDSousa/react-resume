726ef9b084f91d729770fc3f982503c8b1fee472
Glitch (glitch-hello-react) <none>
2023-12-01 17:32:51 +0000

    🚘🌛 Checkpoint ./index.html:59215480/56 ./src/pages/about.jsx:59215480/132 ./src/pages/home.jsx:59215480/108

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,7 +6,9 @@ import { Link } from "wouter";
 export default function Home() {
   return (
     <>
-      
+      <h1 class="text-3xl font-bold underline">
+          Hello world!
+      </h1>
     </>
   );
 }
