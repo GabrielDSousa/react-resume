c4b34a0d2acc38e0dac4d8b797fc9ca232808f4b
Glitch (glitch-hello-react) <none>
2023-12-02 20:37:21 +0000

    🍮💼 Checkpoint ./src/components/resumeHeader.jsx:59215480/2795 ./.glitch-assets:59215480/924

--- src/components/resumeHeader.jsx
+++ src/components/resumeHeader.jsx
@@ -5,12 +5,20 @@ const ResumeHeader = ({ personalInfo }) => {
   const { name, title, location, email, phone } = personalInfo;
 
   return (
-    <div className="mb-8">
-      <div className="text-3xl font-bold">{name}</div>
-      <div className="text-lg">{title}</div>
-      <div className="text-lg">{location}</div>
-      <div className="text-lg">{email}</div>
-      <div className="text-lg">{phone}</div>
+    <div className="mb-8 flex space-between">
+      <img
+        src="https://cdn.glitch.global/f314574e-92fa-4a70-b3ec-f94c4bb038fc/profile.jpg?v=1701548945275"
+        alt="Profile"
+        className="rounded-full object-cover h-32 w-32 mr-4"
+      />
+
+      <div>
+        <div className="text-3xl font-bold">{name}</div>
+        <div className="text-lg">{title}</div>
+        <div className="text-lg">{location}</div>
+        <div className="text-lg">{email}</div>
+        <div className="text-lg">{phone}</div>
+      </div>
     </div>
   );
 };
