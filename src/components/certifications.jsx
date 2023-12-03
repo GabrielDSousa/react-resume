0058e0e6889bea8e25d3cf3b3049535d93d7b9ed
Glitch (glitch-hello-react) <none>
2023-12-03 22:44:09 +0000

    🌸🏭 Checkpoint ./src/components/workExperience.jsx:59215480/21 ./src/components/skills.jsx:59215480/152 ./src/components/education.jsx:59215480/61 ./src/components/certifications.jsx:59215480/40 ./index.html:59215480/349

--- src/components/certifications.jsx
+++ src/components/certifications.jsx
@@ -6,13 +6,13 @@ const Certifications = ({ certifications }) => {
   
   return (
     <div className="mb-8 print:mb-4 p-4">
-      <div className="text-2xl print:text-base font-bold mb-4 print:mb-2 text-lime-950">
+      <div className="text-2xl print:text-base font-bold mb-4 print:mb-2 text-lime-950 dark:text-lime-400">
         {lang === "pt" ? "Certificados" : "Certifications"}
       </div>
       {certifications.map((cert, index) => (
         <div key={index} className={`mb-6 print:mb-3 ${index >= 5 && 'print:hidden'}`}>
           <div className="text-lg print:text-sm font-bold mb-2 print:mb-1">{cert.title}</div>
-          <div className="text-lime-800">
+          <div className="text-lime-800 dark:text-lime-300">
             {cert.issuer} | {cert.issue_date}
           </div>
           {cert.competencies && (
--- src/components/education.jsx
+++ src/components/education.jsx
@@ -7,12 +7,12 @@ const Education = ({ education }) => {
   
   return (
     <div className="mb-8 print:mb-4 p-4">
-      <div className="text-2xl print:text-base text-lime-950 font-bold mb-4 print:mb-2">{lang === "pt" ? "Educação" : "Education"}</div>
+      <div className="text-2xl print:text-base text-lime-950 font-bold mb-4 print:mb-2 dark:text-lime-400">{lang === "pt" ? "Educação" : "Education"}</div>
       {education.map((edu, index) => (
         <div key={index} className="mb-6 print:mb-3">
           <div className="text-lg print:text-sm font-bold mb-2 print:mb-1">{edu.institution}</div>
-          <div className="text-lime-800">{edu.location}</div>
-          <div className="text-lime-800">{edu.date}</div>
+          <div className="text-lime-800 dark:text-lime-300">{edu.location}</div>
+          <div className="text-lime-800 dark:text-lime-300">{edu.date}</div>
           <p className="mb-2 print:mb-1">{edu.degree}</p>
           <p className="text-sm print:text-xs">{edu.description}</p>
         </div>
