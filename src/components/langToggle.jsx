028e673282baea98ca41ed00b455e3e1c15940c0
Glitch (glitch-hello-react) <none>
2023-12-03 23:08:10 +0000

    💎🌴 Checkpoint ./src/components/langToggle.jsx:59215480/1490 ./src/components/menu.jsx:59215480/304 ./src/hooks/LanguageContext.jsx:59215480/4182

--- src/components/langToggle.jsx
+++ src/components/langToggle.jsx
@@ -1,12 +1,24 @@
-// DarkModeToggle.js
 import React from 'react';
 import { useLanguage } from '../hooks/LanguageContext';
 
 const LangToggle = () => {
-  const { lang } = useLanguage();
-  
+  const { lang, setLang } = useLanguage();
+
+  const toggleLanguage = () => {
+    const newLang = lang === 'en' ? 'pt' : 'en';
+
+    // Update language state
+    setLang(newLang);
+
+    // Update URL without a reload
+    const newUrl = window.location.pathname + window.location.search + `?lang=${newLang}`;
+    window.history.pushState({ path: newUrl }, '', newUrl);
+  };
+
   return (
-    
+    <button onClick={toggleLanguage}>
+      {lang === 'pt' ? 'Trocar para Inglês'}
+    </button>
   );
 };
 
--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Link } from "wouter";
 import DarkModeToggle from "./darkModeToggle"
+import LangToggle from "./langToggle"
 
 const Menu = ({ isDarkMode, toggleDarkMode }) => {
   return (   
     <div className="bg-gray-800 text-white p-4 mb-4 print:hidden">
       <div className="flex justify-between items-center">
         <div>
-          <ul className="flex space-x-4">
-            <li><Link href="/" className="hover:text-gray-300">Portuguese</Link></li>
-            <li><Link href="/en" className="hover:text-gray-300">English</Link></li>
-          </ul>
+          <LangToggle />
         </div>
         <div>
           <DarkModeToggle isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
