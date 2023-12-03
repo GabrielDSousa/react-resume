8761cf038cc424244dd6c990b8049a14806a9ccb
Glitch (glitch-hello-react) <none>
2023-12-03 19:56:08 +0000

    🍥🦃 Checkpoint ./src/resume-pt.json:59215480/47916 ./src/components/certifications.jsx:59215480/4232 ./src/pages/home.jsx:59215480/8

--- src/components/certifications.jsx
+++ src/components/certifications.jsx
@@ -1,15 +1,25 @@
-// Certifications.js
 import React from 'react';
 
 const Certifications = ({ certifications }) => {
   return (
     <div className="mb-8 print:mb-4">
-      <div className="text-2xl print:text-base font-bold mb-4 print:mb-2 text-lime-950">Certifications</div>
+      <div className="text-2xl print:text-base font-bold mb-4 print:mb-2 text-lime-950">
+        Certifications
+      </div>
       {certifications.map((cert, index) => (
         <div key={index} className="mb-6 print:mb-3">
           <div className="text-lg print:text-sm font-bold mb-2 print:mb-1">{cert.title}</div>
-          <div className="text-lime-800">{cert.institution} | {cert.location} | {cert.date}</div>
-          <p>{cert.description}</p>
+          <div className="text-lime-800">
+            {cert.issuer} | {cert.issue_date}
+          </div>
+          {cert.competencies && (
+            <div className="text-lime-500">
+              Competencies: {cert.competencies.join(', ')}
+            </div>
+          )}
+          {cert.credential_code && (
+            <div className="text-lime-500">Credential Code: {cert.credential_code}</div>
+          )}
         </div>
       ))}
     </div>
