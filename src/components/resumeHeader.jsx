429342e3e02e2964c52252dd5094fecdf1b45a51
Glitch (glitch-hello-react) <none>
2023-12-03 16:30:12 +0000

    🚛🍽 Checkpoint ./src/components/resumeHeader.jsx:59215480/6696 ./src/components/workExperience.jsx:59215480/24

--- src/components/resumeHeader.jsx
+++ src/components/resumeHeader.jsx
@@ -17,9 +17,36 @@ const ResumeHeader = ({ personalInfo }) => {
       <div>
         <div className="text-3xl font-bold">{name}</div>
         <div className="text-lg">{title}</div>
-        <div className="text-lg"><i className="fa fa-map-marker pl-1 pr-3"></i>{location}</div>
-        <div className="text-lg"><i className="fa fa-envelope pr-2"></i>{email}</div>
-        <div className="text-lg"><i className="fa fa-phone pr-2"></i>{phone}</div>
+        <div className="text-lg flex items-center">
+          <i className="fa fa-map-marker pr-3"></i>
+          {location}
+        </div>
+        <div className="text-lg flex items-center">
+          <i className="fa fa-envelope pr-2"></i>
+          {email}
+        </div>
+        <div className="text-lg flex items-center">
+          <i className="fa fa-phone pr-2"></i>
+          {phone}
+        </div>
+      </div>
+
+      {/* Social Links Grid */}
+      <div className="flex flex-col">
+        <div className="text-lg flex items-center">
+          <i className="fa fa-linkedin pr-2"></i>
+          <span className="inline print:hidden">
+            <a href="https://www.linkedin.com/in/GabrielDSousa/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          </span>
+          <span className="hidden print:inline">https://www.linkedin.com/in/GabrielDSousa/</span>
+        </div>
+        <div className="text-lg flex items-center">
+          <i className="fa fa-github pr-2"></i>
+          <span className="inline print:hidden">
+            <a href="https://github.com/GabrielDSousa" target="_blank" rel="noopener noreferrer">GitHub</a>
+          </span>
+          <span className="hidden print:inline">https://github.com/GabrielDSousa</span>
+        </div>
       </div>
     </div>
   );
--- src/components/workExperience.jsx
+++ src/components/workExperience.jsx
@@ -6,11 +6,11 @@ const WorkExperience = ({ experience }) => {
   
   return (
     <div className="mb-8">
-      <div className="text-2xl font-bold mb-4">{ lang === "pt" ? "Experiência profissional" : "Work Experience"}</div>
+      <div className="text-2xl font-bold mb-4 text-lime-950">{ lang === "pt" ? "Experiência profissional" : "Work Experience"}</div>
       {experience.map((job, index) => (
         <div key={index} className="mb-6">
           <div className="text-lg font-bold mb-2">{job.title}</div>
-          <div className="text-gray-300">{job.company} - {job.location} | {job.date}</div>
+          <div className="text-lime-800">{job.company} - {job.location} | {job.date}</div>
           <p className="mb-2">{job.description}</p>
           {job.milestones && (
             <ul className="list-disc pl-6">
